5dd7669d2ff2fdfa7b445888a249acc27cbb55b1
Anthony <anthony.ae@outlook.com>
2022-11-04 21:44:46 -0700

    added more h files

---
+++ config/keymap_hm_speed_settings.h
@@ -0,0 +1,16 @@
+
+/*
+* Home row speed settings 
+*/
+
+#define MEH LS(LA(LCTRL))
+
+#define hms_base 400
+#define hms_base_high_resistance_keycaps 200
+#define HMS_VVSLO (hms_base + 300)
+#define HMS_VSLO (hms_base + 100)
+#define HMS_SLOW (hms_base + 50)
+#define HMS_EXP  (hms_base + 20)
+#define HMS_MEDI (hms_base - 50)
+#define HMS_FAST (hms_base - 150)
+#define HMS_VFAS 130
